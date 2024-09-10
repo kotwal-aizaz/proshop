2e42b380fa8608ce728d4ce6ff0bfbca88a869f3
Aizaz Kotwal <aizazkotwal2204@outlook.com>
2024-09-10 14:52:05 +0530

    feat: implement header,footer and homescreen components

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,5 +1,18 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import { Container } from "react-bootstrap";
+import HomeScreen from "./screens/HomeScreen";
 const App = () => {
-  return <>test</>;
+  return <>
+
+    <Header/>
+    <main className="py-3">
+      <Container>
+        <HomeScreen/>
+      </Container>
+    </main>
+    <Footer/>
+  </>;
 };
 
 export default App;
