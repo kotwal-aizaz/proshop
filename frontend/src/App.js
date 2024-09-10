e96a9ea07d54501aec214cde93a53609345c6007
Aizaz Kotwal <aizazkotwal2204@outlook.com>
2024-09-10 15:07:14 +0530

    feat: implement react router

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,18 +1,19 @@
+import { Outlet } from "react-router-dom";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
 import { Container } from "react-bootstrap";
-import HomeScreen from "./screens/HomeScreen";
 const App = () => {
-  return <>
-
-    <Header/>
-    <main className="py-3">
-      <Container>
-        <HomeScreen/>
-      </Container>
-    </main>
-    <Footer/>
-  </>;
+  return (
+    <>
+      <Header />
+      <main className="py-3">
+        <Container>
+          <Outlet />
+        </Container>
+      </main>
+      <Footer />
+    </>
+  );
 };
 
 export default App;
