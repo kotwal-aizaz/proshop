3ab4f2e2e4c5310aa8988563822908a802ef38b1
Aizaz Kotwal <aizazkotwal2204@outlook.com>
2024-09-11 15:30:26 +0530

    chore: clean up product screen

--- frontend/src/screens/ProductScreen.jsx
+++ frontend/src/screens/ProductScreen.jsx
@@ -43,20 +43,24 @@ const ProductScreen = () => {
                   </Col>
                 </Row>
               </ListGroup.Item>
-              
+
               <ListGroup.Item>
                 <Row>
                   <Col>Status:</Col>
                   <Col>
                     <strong>
-                      ${product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
+                      {product.countInStock > 0 ? "In Stock" : "Out Of Stock"}
                     </strong>
                   </Col>
                 </Row>
               </ListGroup.Item>
               <ListGroup.Item>
-                <Button className="btn-block" type="button" disabled={product.countInStock === 0}>
-                    Add To Cart
+                <Button
+                  className="btn-block"
+                  type="button"
+                  disabled={product.countInStock === 0}
+                >
+                  Add To Cart
                 </Button>
               </ListGroup.Item>
             </ListGroup>
