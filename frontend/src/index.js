8a6e20e9fcf2ff94172d5d05f12fa34e0490e80e
Aizaz Kotwal <aizazkotwal2204@outlook.com>
2024-09-11 14:16:11 +0530

    feat: implement product details screen

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -7,16 +7,18 @@ import {
   RouterProvider,
 } from "react-router-dom";
 // import "bootstrap/dist/css/bootstrap.min.css";
-import "./bootstrap.custom.css"
+import "./bootstrap.custom.css";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
 import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
 
 const router = createBrowserRouter(
   createRoutesFromElements(
     <Route path="/" element={<App />}>
       <Route index path="/" element={<HomeScreen />} />
+      <Route path="/product/:id" element={<ProductScreen />} />
     </Route>
   )
 );
