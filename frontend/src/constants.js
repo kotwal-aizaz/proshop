a6ab0ac1909c78b708a2dc45193be0b8c35cd638
Aizaz Kotwal <aizazkotwal2204@outlook.com>
2024-09-17 16:29:25 +0530

    feat: add redux store with RTK Query

---
+++ frontend/src/constants.js
@@ -0,0 +1,7 @@
+// export const BASE_URL =
+//   process.env.NODE_ENV === "development" ? "http://localhost:5000" : "";
+export const BASE_URL = "" // since we have a proxy url set up in the package.json we don't need to set the base url
+export const PRODUCTS_URL = "/api/products";
+export const USERS_URL = "/api/users";
+export const ORDERS_URL = "/api/orders";
+export const PAYPAL_URL = "/api/config/paypal";
